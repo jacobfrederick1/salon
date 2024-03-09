89fe0937c0787bb66edece0406528f00d4841cf1
Jacob Frederick <60794125+jacobfrederick1@users.noreply.github.com>
2024-03-09 00:54:01 -0600

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -2,68 +2,67 @@
 #Salon Script
 PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c"
 echo -e "\n\n~~Jacob's Salon~~\n\n"
-
+  
+echo -e "\nWelcome to Jacob's Salon, how can I help you?"
 
 MAIN_MENU(){
-
   #List salon services
   SERVICES_RESULT=$($PSQL "Select * FROM services;")
-  echo -e "\n$SERVICES_RESULT\n" | sed 's/|/) /g'
+  echo -e "\n$SERVICES_RESULT" | sed 's/|/) /g'
 
-  echo -e "\nWhat are you looking to do today?"
-  read CUSTOMER_SERVICE
+  read SERVICE_ID_SELECTED
 
   #Identify what the customer wants done
-  CUSTOMER_SERVICE_RESULT=$($PSQL "Select name FROM services WHERE service_id=$CUSTOMER_SERVICE;")
+  SERVICE_ID_SELECTED_RESULT=$($PSQL "Select name FROM services WHERE service_id=$SERVICE_ID_SELECTED;")
   
-  if [[ -z $CUSTOMER_SERVICE_RESULT ]]
+  if [[ -z $SERVICE_ID_SELECTED_RESULT ]]
     then
-      echo -e "\nPlease select one of the available options"
+      echo -e "\nI could not find that service. What would you like today?"
       MAIN_MENU
     else
       echo -e "\nWhat is your phone number?"
-      read PHONE_NUMBER 
-      if [[ $PHONE_NUMBER =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]
+      read CUSTOMER_PHONE 
+      if [[ $CUSTOMER_PHONE  =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]
         then
-          CUSTOMER_PHONE_RESULT=$($PSQL "Select customer_id FROM customers WHERE phone='$PHONE_NUMBER';")
-          echo $CUSTOMER_PHONE_RESULT
+          CUSTOMER_PHONE_RESULT=$($PSQL "Select customer_id FROM customers WHERE phone='$CUSTOMER_PHONE';")
           #Create a customer if they do not exist
           if [[ -z $CUSTOMER_PHONE_RESULT ]]
             then
-                echo -e "\nWhat is your name?"
-                read NAME
-                INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name,phone) VALUES('$NAME','$PHONE_NUMBER');")
+                echo -e "\nI don't have a record for that phone number, what's your name?"
+                read CUSTOMER_NAME
+                INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE');")
                 if [[ $INSERT_CUSTOMER_RESULT == "INSERT 0 1" ]]
                   then
-                    echo Customer successfully added
-                    CUSTOMER_PHONE_RESULT=$($PSQL "Select customer_id FROM customers WHERE phone='$PHONE_NUMBER';")
-                    CREATE_APPOINTMENT $PHONE_NUMBER $CUSTOMER_PHONE_RESULT $CUSTOMER_SERVICE $APPOINTMENT_TIME
+                    CUSTOMER_PHONE_RESULT=$($PSQL "Select customer_id FROM customers WHERE phone='$CUSTOMER_PHONE';")
+                    CREATE_APPOINTMENT $CUSTOMER_PHONE $CUSTOMER_PHONE_RESULT $SERVICE_ID_SELECTEDE
                 else
                   echo Customer failed to be added
                 fi
             else
-              CREATE_APPOINTMENT $PHONE_NUMBER $CUSTOMER_PHONE_RESULT $CUSTOMER_SERVICE $APPOINTMENT_TIME
+              CREATE_APPOINTMENT $CUSTOMER_PHONE $CUSTOMER_PHONE_RESULT $SERVICE_ID_SELECTED
           fi
         else
-          echo -e "\nPhone number does not match form \#\#\#-\#\#\#-\#\#\#\#"
+          echo -e "\nPhone number did not match the form: ###-###-####"
           MAIN_MENU
       fi
   fi
 }
+
+#Create an appointment
 CREATE_APPOINTMENT(){
   #CREATE AN appointment
-    echo -e "\nWhat time would you like to come in?"
-    read APPOINTMENT_TIME
+    CUSTOMER_NAME=$($PSQL "Select name FROM customers WHERE phone='$CUSTOMER_PHONE';")
+   
+    echo -e "\nWhat time would you like your $SERVICE_ID_SELECTED_RESULT, $CUSTOMER_NAME?\n"
+    read SERVICE_TIME
 
-    NAME=$($PSQL "Select name FROM customers WHERE phone='$PHONE_NUMBER';")
-    INSERT_CUSTOMER_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES('$CUSTOMER_PHONE_RESULT','$CUSTOMER_SERVICE','$APPOINTMENT_TIME');")
+    INSERT_CUSTOMER_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES('$CUSTOMER_PHONE_RESULT','$SERVICE_ID_SELECTED','$SERVICE_TIME');")
 
     if [[ $INSERT_CUSTOMER_APPOINTMENT == "INSERT 0 1" ]]
       then
-        echo -e "I have put you down for a $CUSTOMER_SERVICE_RESULT at $APPOINTMENT_TIME, $NAME."
+        echo -e "\nI have put you down for a $SERVICE_ID_SELECTED_RESULT at $SERVICE_TIME, $CUSTOMER_NAME.\n"
     else
       echo -e "\nAppointment could not be schedueled"
-    MAIN_MENU
     fi
 }
 MAIN_MENU
